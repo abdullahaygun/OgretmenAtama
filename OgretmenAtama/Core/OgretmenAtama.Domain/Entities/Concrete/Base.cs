0d5a0fd2cb3da4abf8936eea8e2597dd22625c60
Abdullah AYGÜN <ayguns.abdullah@gmail.com>
2022-07-31 19:46:39 +0300

    init ettim.

---
+++ OgretmenAtama/Core/OgretmenAtama.Domain/Entities/Concrete/Base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgretmenAtama.Domain.Entities.Concrete
+{
+    public class Base
+    {
+        private Guid _Id;
+        private DateTime _CreatedDate;
+        private DateTime _ModifiedDate;
+        private bool _IsActive;
+
+        public Guid Id { get => _Id; set => _Id = value; }
+        public DateTime CreatedDate { get => _CreatedDate; set => _CreatedDate = value; }
+        public DateTime ModifiedDate { get => _ModifiedDate; set => _ModifiedDate = value; }
+        public bool IsActive { get => _IsActive; set => _IsActive = value; }
+    }
+}
---
+++ OgretmenAtama/Infrastructure/OgretmenAtama.Persistance/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OgretmenAtama.Persistance.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgretmenAtama.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            ConfigurationManager configurationManager = new();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory()));
+            configurationManager.AddJsonFile("appsettings.json");
+
+            services.AddDbContext<OgretmenAtamaDbContext>(options => options.UseNpgsql(configurationManager.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
