15ea2e25dad07376e5fb4584a8dd807569f76b9d
Abdullah AYGÜN <ayguns.abdullah@gmail.com>
2022-08-01 00:19:25 +0300

    ..

---
+++ OgretmenAtama/Presentation/OgretmenAtama.API/Controllers/DBController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Office.Interop.Excel;
+using OgretmenAtama.Persistance.Data.Contexts;
+using System.Diagnostics;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace OgretmenAtama.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DBController : ControllerBase
+    {
+        private readonly OgretmenAtamaDbContext _context;
+
+        public DBController(OgretmenAtamaDbContext context)
+        {
+            _context = context;
+        }
+
+        #region deneme
+        [Route("[action]")]
+        [HttpGet]
+        public async Task<char> Test(string filepath)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new();
+            Workbook wb;
+            Worksheet ws;
+
+            wb = excel.Workbooks.Open(filepath);
+            ws = (Worksheet)wb.Worksheets[1];
+
+            Range cell = ws.Range["A1:A2"];
+
+            foreach (var val in cell.Value.ToString())
+            {
+                Debug.WriteLine("test: " + val);
+                return val;
+            }
+            return '0';
+
+        }
+        #endregion
+    }
+}
