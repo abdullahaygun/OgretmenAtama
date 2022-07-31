ec654209f9541445e825d426acd3a7e7b7b3c15c
Abdullah AYGÜN <ayguns.abdullah@gmail.com>
2022-07-31 22:36:00 +0300

    ..

--- OgretmenAtama/Core/OgretmenAtama.Domain/Entities/Concrete/KurumlarAlanlar.cs
+++ OgretmenAtama/Core/OgretmenAtama.Domain/Entities/Concrete/KurumlarAlanlar.cs
@@ -6,12 +6,9 @@ using System.Threading.Tasks;
 
 namespace OgretmenAtama.Domain.Entities.Concrete
 {
-    public class Kurumlar:Base
+    public class KurumlarAlanlar:Base
     {
         public int KurumKodu { get; set; }
-        public string KurumAdi { get; set; }
-        public byte KurumHizmetAlani { get; set; }
-        public byte KurumTipi { get; set; }
-        public int BosKontenjan { get; set; }
+        public int AlanKodu { get; set; }
     }
 }
---
+++ OgretmenAtama/Infrastructure/OgretmenAtama.Persistance/Data/Migrations/20220731193016_mig_1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OgretmenAtama.Persistance.Migrations
+{
+    public partial class mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alanlar",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AlanKodu = table.Column<int>(type: "integer", nullable: false),
+                    AlanAdi = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alanlar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ilceler",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IlKodu = table.Column<byte>(type: "smallint", nullable: false),
+                    IlceAdi = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ilceler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Iller",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IlKodu = table.Column<byte>(type: "smallint", nullable: false),
+                    IlAdi = table.Column<string>(type: "text", nullable: false),
+                    IlHizmetBolgesi = table.Column<byte>(type: "smallint", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Iller", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kurumlar",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    KurumKodu = table.Column<int>(type: "integer", nullable: false),
+                    KurumAdi = table.Column<string>(type: "text", nullable: false),
+                    AlanKodu = table.Column<int>(type: "integer", nullable: false),
+                    KurumHizmetAlani = table.Column<byte>(type: "smallint", nullable: false),
+                    KurumTipi = table.Column<byte>(type: "smallint", nullable: false),
+                    BosKontenjan = table.Column<int>(type: "integer", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kurumlar", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alanlar");
+
+            migrationBuilder.DropTable(
+                name: "Ilceler");
+
+            migrationBuilder.DropTable(
+                name: "Iller");
+
+            migrationBuilder.DropTable(
+                name: "Kurumlar");
+        }
+    }
+}
