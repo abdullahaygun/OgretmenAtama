9373b824cdd230cc512773ac4a2a205734cb21a3
Abdullah AYGÜN <ayguns.abdullah@gmail.com>
2022-07-31 20:09:36 +0300

    Revert "Delete OgretmenAtama directory"
    This reverts commit dc56e2723451093150fec0acc201bf916332ecbe.

---
+++ OgretmenAtama/Core/OgretmenAtama.Domain/Entities/Concrete/Alanlar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgretmenAtama.Domain.Entities.Concrete
+{
+    public class Alanlar:Base
+    {
+        public int AlanKodu { get; set; }
+        public string AlanAdi { get; set; }
+    }
+}
---
+++ OgretmenAtama/Core/OgretmenAtama.Domain/Entities/Concrete/Ilceler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgretmenAtama.Domain.Entities.Concrete
+{
+    public class Ilceler:Base
+    {
+        public byte IlKodu { get; set; }
+        public string IlceAdi { get; set; }
+    }
+}
